3dce4d3c8d9a1d8301979ea1e5d86ea064671974
James <44986932+JamesNZL@users.noreply.github.com>
2022-05-16 14:09:09 +1200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '*',
+        destination: 'https://github.com/JamesNZL',
+        permanent: false,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
