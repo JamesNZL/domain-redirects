6140d25a686bb9c761a06723c8ef76539f562b95
James <44986932+JamesNZL@users.noreply.github.com>
2022-05-16 14:09:39 +1200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: '*',
+        source: '/*',
         destination: 'https://github.com/JamesNZL',
         permanent: false,
       },
